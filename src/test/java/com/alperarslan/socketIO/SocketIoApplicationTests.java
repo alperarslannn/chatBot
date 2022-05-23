f38fc6fe6c1b7f48f5265593182ab4101d31f7ff
alperarslannn <alper.arslan663@gmail.com>
2022-05-23 21:16:52 +0300

    Backend Server Prep - Init

---
+++ src/test/java/com/alperarslan/socketIO/SocketIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperarslan.socketIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
