af392b939583bde82d30d7fb3367c13f1d9e62bf
alperarslannn <alper.arslan663@gmail.com>
2022-05-24 22:12:51 +0300

    Frontend - Backend Connection Established, UI mostly done.

--- src/main/java/com/alperarslan/socketIO/Controller/ChatController.java
+++ src/main/java/com/alperarslan/socketIO/Controller/ChatController.java
@@ -1,25 +1,64 @@
 package com.alperarslan.socketIO.Controller;
 
+import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIONamespace;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-@Component
+import java.util.EventListener;
+
+@Controller
 public class ChatController {
     private SocketIONamespace namespace;
 
+    public SocketIONamespace getNamespace(){
+        return namespace;
+    }
+
+    private SocketIOServer server;
+
+    public SocketIOServer getServer(){
+        return server;
+    }
+
     @Autowired
     public ChatController(SocketIOServer server){
-        namespace = server.addNamespace("/chat");
-
-        namespace.addConnectListener(new ConnectListener() {
-            @Override
-            public void onConnect(SocketIOClient socketIOClient) {
-                System.out.println("Client connected to /chat");
-            }
-        });
+        this.server = server;
+        //namespace = server.addNamespace("http://localhost:8000/chat");
+        //System.out.println(namespace.getName());
+        //this.namespace.addConnectListener(onConnectListener);
+        //this.namespace.addDisconnectListener(onDisconnectListener);
+        server.addConnectListener(onConnectListener);
+        server.addDisconnectListener(onDisconnectListener);
+        server.addEventListener("chat", String.class, onUserSendMessage);
+        //this.namespace.addEventListener("chat", String.class, dataListener);
     }
+
+    public ConnectListener onConnectListener = new ConnectListener() {
+        @Override
+        public void onConnect(SocketIOClient socketIOClient) {
+            System.out.println("Client " + socketIOClient.getSessionId() + " connected to /chat namespace");
+        }
+    };
+
+    public DisconnectListener onDisconnectListener = new DisconnectListener() {
+        @Override
+        public void onDisconnect(SocketIOClient socketIOClient) {
+            System.out.println("Client " + socketIOClient.getSessionId() + " disconnected from /chat namespace");
+            //namespace.getBroadcastOperations().sendEvent();
+        }
+    };
+
+    public DataListener<String> onUserSendMessage = new DataListener<String>() {
+        @Override
+        public void onData(SocketIOClient socketIOClient, String message, AckRequest ackRequest) throws Exception {
+            getServer().getBroadcastOperations().sendEvent("chat", socketIOClient, message);
+            System.out.println(message);
+        }
+    };
 }
--- src/main/java/com/alperarslan/socketIO/SocketIoApplication.java
+++ src/main/java/com/alperarslan/socketIO/SocketIoApplication.java
@@ -2,9 +2,16 @@ package com.alperarslan.socketIO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocketIoApplication {
+public class SocketIoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SocketIoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocketIoApplication.class, args);
