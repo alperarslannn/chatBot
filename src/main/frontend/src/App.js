bf195275e66df0741507744141cf1173194a672e
a.arslan <a.arslan@cekino.com>
2022-05-24 22:12:51 +0300

    Frontend - Backend Connection Established, UI mostly done.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,21 @@
 import { Button, TextField, Box } from '@mui/material';
 import { useEffect, useState } from 'react';
+//import * as io from 'socket.io-client';
+import io from 'socket.io-client';
 import './App.css';
+
+// const socket = io({
+//   path: 'http://localhost:8000/chat',
+//   transports: ['polling', 'websocket'],
+// });
+
+const socket = io.connect('http://localhost:8000');
+//let chatSok = io.connect('http://localhost:8000/chat');
+
 
 function App() {
 
   const [message, setMessage] = useState('');
-
 
   let sendMessageWithEnter = (e) => {
     if(e.keyCode === 13){
@@ -34,13 +44,44 @@ function App() {
 
 
   let getBotAnswer = () => {
+    // socket.on("newMessage", function(message) {
+		// 	displayMessage(message.user.username, message.message);
+		// });
 
+    console.log("bot func")
+    console.log(socket.connected);
+
+    // socket.on("connection", (socket) => {
+    //   console.log("succeed");
+    //   socket.emit("frontendSaysHi");
+    // });
+
+    socket.emit("chat","HelloWorld");
+
+    // socket.on("chat", function(data){
+    //   console.log("data");
+    // });
+    
+
+    // chatSok.on("chat", (socket) => {
+    //   console.log("succeed");
+    //   socket.emit("frontendSaysHi".toString);
+    // });
   }
 
   useEffect(() => {
-    
+    socket.on("connect", function(){
+      console.log("connected");
+      alert("connected");
+    } )
+
+    //   chatSok.on("connect", function(){
+    //   console.log("connectedToChatSok");
+    //   alert("connectedToChatSok");
+    // } )
   },[])
 
+  
 
 
   const handleMessageChange = (event) => {
@@ -50,20 +91,8 @@ function App() {
   return (
     <div className="App">
       <h1 className='App-header'>Chat - Bot</h1>
-      <Box sx={{  m:10, border: '1px solid grey', padding: "1px 1px 1px 1px", margin: "1% 10%", height:"75vh", borderRadius:"10px"}}>
+      <Box className={"chatContainer"} sx={{  m:10, border: '1px solid grey', padding: "1px 1px 1px 1px", margin: "1% 10%", height:"75vh", borderRadius:"10px"}}>
         <Box sx={{ margin: "3% 5% 0px 5%", border: '1px solid grey', maxHeight: "80%", minHeight:"80%" ,overflow:"auto" }} className={"chatBox"} >
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
-          <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
-          <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
           <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
           <div className='serverText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX  aaaaaaaaaaaa aaaaaaaaaaaaaaaaaa</div>
           <div className='clientText chatText'>HiAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX </div>
@@ -83,10 +112,12 @@ function App() {
             margin: "5px 5%"
           }}
         >
-          <TextField  id="outlined-basic" label="Message" variant="outlined" value={message} onChange={handleMessageChange} onKeyDown={e => sendMessageWithEnter(e)} />
+          <TextField sx={{
+            backgroundColor:'#ffe5ec', color:'white', border:"1px solid grey", borderRadius:'10px'
+          }}  id="outlined-basic" label="Message" value={message} onChange={handleMessageChange} onKeyDown={e => sendMessageWithEnter(e)} />
           <Button
           sx={{
-            m: 1, width: '5%', height:'7ch', ml:0
+            m: 1, width: '5%', height:'7ch', ml:0, backgroundColor:'#b388eb', color:'white',borderColor:'#fdc5f5'
           }}
           variant="outlined" onClick={sendMessage}>Send</Button>
         </Box>
--- src/main/java/com/alperarslan/socketIO/Configuration/SocketIOServerConfiguration.java
+++ src/main/java/com/alperarslan/socketIO/Configuration/SocketIOServerConfiguration.java
@@ -1,33 +1,59 @@
 package com.alperarslan.socketIO.Configuration;
 
+import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PreDestroy;
 
 @org.springframework.context.annotation.Configuration
 public class SocketIOServerConfiguration {
-    private static final String SOCKET_HOST = "0.0.0.0";
+    private static final String SOCKET_HOST = "localhost";
     private static final int SOCKET_PORT = 8000;
 
     private SocketIOServer server;
 
-    public SocketIOServerConfiguration(){
+    @Bean
+    public SocketIOServer ServerConfiguration(){
         Configuration configuration = new Configuration();
         configuration.setHostname(SOCKET_HOST);
         configuration.setPort(SOCKET_PORT);
+        configuration.setOrigin("http://localhost:3000");
 
         server = new SocketIOServer(configuration);
+
         server.start();
 
-        server.addConnectListener(new ConnectListener() {
+
+       /*server.addConnectListener(new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient socketIOClient) {
                 System.out.println("Client Connected " + socketIOClient.getSessionId());
             }
         });
+
+        server.addDisconnectListener(new DisconnectListener() {
+            @Override
+            public void onDisconnect(SocketIOClient socketIOClient) {
+                System.out.println("Client disconnected: " + socketIOClient.getSessionId());
+            }
+        });*/
+
+        /*server.addEventListener("chat", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient socketIOClient, String message, AckRequest ackRequest) throws Exception {
+                server.getBroadcastOperations().sendEvent("chat", socketIOClient, message);
+                System.out.println(message);
+            }
+        });*/
+
+        return server;
     }
 
     @PreDestroy
